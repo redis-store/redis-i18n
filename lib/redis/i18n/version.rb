a16dd540bf050a287dc692622f3eef02f1a88d4f
Tom Scott <tscott@weblinc.com>
2020-08-11 14:20:58 -0400

    v0.8: Update i18n dependency

--- lib/redis/i18n/version.rb
+++ lib/redis/i18n/version.rb
@@ -1,5 +1,5 @@
 class Redis
   module I18n
-    VERSION = '0.7.0'
+    VERSION = '0.8.0'
   end
 end
--- test/redis/i18n/version_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-describe Redis::I18n::VERSION do
-  it 'returns current version' do
-    Redis::I18n::VERSION.must_equal '0.7.0'
-  end
-end
